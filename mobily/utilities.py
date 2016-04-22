8f8cd222df39fb0f8ced40fc5be174e816e08b56
Lucas Thompson <dev@lucas.im>
2016-04-22 14:50:52 +0700

    Updated code to ensure strings are unicode, and changed tests to implement my current understanding of how the mobily unicode encoding is.

--- mobily/utilities.py
+++ mobily/utilities.py
@@ -1,6 +1,15 @@
+import sys
+
+
 class MobilyUnicodeEncoder:
     def __init__(self, message):
         self.message = message
+        self._ensure_unicode()
 
     def encode(self):
         return ''.join(['{:04x}'.format(ord(byte)).upper() for byte in self.message])
+
+    def _ensure_unicode(self):
+        if sys.version_info < (3,):
+            if type(self.message) is not unicode:
+                self.message = unicode(self.message, 'utf-8')
--- tests/utilities/test_mobily_unicode.py
+++ tests/utilities/test_mobily_unicode.py
@@ -4,17 +4,25 @@ from mobily.utilities import MobilyUnicodeEncoder
 
 
 class TestMobilyUnicode(unittest.TestCase):
-    def test_multiple_bytes(self):
-        encoder = MobilyUnicodeEncoder('•')
-        self.assertEqual('00E2008000A2', encoder.encode())
-        encoder = MobilyUnicodeEncoder('π')
-        self.assertEqual('00CF0080', encoder.encode())
-
-    def test_single_byte_with_leading_zeros(self):
+    def test_single_byte_characters_with_leading_zeros(self):
         encoder = MobilyUnicodeEncoder('\r')
         self.assertEqual('000D', encoder.encode())
         encoder = MobilyUnicodeEncoder('M')
         self.assertEqual('004D', encoder.encode())
+
+    def test_multi_byte_characters(self):
+        encoder = MobilyUnicodeEncoder('•')
+        self.assertEqual('2022', encoder.encode())
+        encoder = MobilyUnicodeEncoder('π')
+        self.assertEqual('03C0', encoder.encode())
+
+    def test_emoji_characters_for_fun(self):
+        grin_face = '😁'
+        encoder = MobilyUnicodeEncoder(grin_face)
+        self.assertEqual('D83DDE01', encoder.encode())
+        pile_of_poo = '💩'
+        encoder = MobilyUnicodeEncoder(pile_of_poo)
+        self.assertEqual('D83DDCA9', encoder.encode())
 
     def test_arabic_string_from_api_docs(self):
         encoder = MobilyUnicodeEncoder('اهلا وسهلا بك مع mobily.ws')
