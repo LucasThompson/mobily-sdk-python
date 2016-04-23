fbdfd7c4468b563304ee1e0a92200b36ff7993dd
Lucas Thompson <dev@lucas.im>
2016-04-23 18:09:23 +0700

    Updated api request tests to use the new MobilyAuth

--- tests/utilities/test_mobily_api_request.py
+++ tests/utilities/test_mobily_api_request.py
@@ -2,7 +2,8 @@
 import unittest
 from mobily.utilities import MobilyApiRequest
 from mobily.utilities import MobilyApiResponse
-import xml.etree.ElementTree as ET
+from mobily.account import MobilyAuth
+
 
 class TestMobilyApiRequest(unittest.TestCase):
     def test_xml_building(self):
@@ -10,12 +11,12 @@ class TestMobilyApiRequest(unittest.TestCase):
         expected_xml += '<Auth mobile="66902258638" password="ThaiSim2016" />'
         expected_xml += '<Method>balance</Method>'
         expected_xml += '</MobilySMS>'
-        request = MobilyApiRequest('66902258638', 'ThaiSim2016')
+        request = MobilyApiRequest(MobilyAuth('66902258638', 'ThaiSim2016'))
         request.add_parameter('Method', 'balance')
         self.assertEqual(expected_xml, request.get_request_data())
 
     def test_balance_request(self):
-        request = MobilyApiRequest('66902258638', 'ThaiSim2016')
+        request = MobilyApiRequest(MobilyAuth('66902258638', 'ThaiSim2016'))
         request.add_parameter('Method', 'balance')
         expected_response = MobilyApiResponse('1', 'success')
         expected_response.add_data('balance', {'current': '1', 'total': '3'})
