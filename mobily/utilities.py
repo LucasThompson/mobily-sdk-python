d0064ba631e8c4f34fddf1043b11cc9cdd6735af
Lucas Thompson <dev@lucas.im>
2016-04-25 21:18:25 +0700

    All RequestHandlers but JSON removed

--- mobily/utilities.py
+++ mobily/utilities.py
@@ -6,8 +6,6 @@ Utility functions to support the Mobily API.
 """
 import sys
 import httplib
-import urllib
-import xml.etree.ElementTree as ET
 import json
 
 
@@ -79,46 +77,13 @@ class MobilyApiRequest(object):
         return data
 
 
-class MobilyApiHttpRequestHandler(object):
-    def __init__(self, auth=None, request=MobilyApiRequest(api_end_point='/api/')):
-        self.request = request
-        self.auth = auth
-        self.content_type = 'x-www-form-urlencoded'
-        self.params = {}
-        self.error_predicate = None
-        self.error = None
-        self.add_auth(auth)
-
-    def add_auth(self, auth):
-        if isinstance(auth, MobilyAuth):
-            self.add_parameter('mobile', auth.mobile_number)
-            self.add_parameter('password', auth.password)
-
-    def set_api_method(self, method_name):
-        self.request.api_end_point = '/api/{0}.php'.format(method_name)
-
-    def add_parameter(self, key, value):
-        if value is not None:
-            self.params.update({key: value})
-
-    def get_request_data(self):
-        return urllib.urlencode(self.params)
-
-    def handle(self):
-        return self._parse_response(self.request.send(self.get_request_data(), self.content_type))
-
-    def _parse_response(self, data):
-        if callable(self.error_predicate) and self.error_predicate(data):
-            raise self.error
-        return data
-
-
-class MobilyApiJsonRequestHandler(MobilyApiHttpRequestHandler):
+class MobilyApiJsonRequestHandler(object):
     def __init__(self, auth=None, request=MobilyApiRequest(api_end_point='/api/json/')):
-        super(MobilyApiJsonRequestHandler, self).__init__(request=request)
+        self.request = request
+        self.params = {}
         self.content_type = 'json'
-        self.auth = auth
         self.json_dict = {'Data': {}}
+        self.auth = auth
 
     def add_auth(self, auth):
         if isinstance(auth, MobilyAuth):
@@ -127,13 +92,21 @@ class MobilyApiJsonRequestHandler(MobilyApiHttpRequestHandler):
     def set_api_method(self, method_name):
         self.json_dict['Data'].update({'Method': method_name})
 
+    def add_parameter(self, key, value):
+        if value is not None:
+            self.params.update({key: value})
+
     def get_request_data(self):
         self.add_auth(self.auth)
         if len(self.params) > 0:
             self.json_dict['Data'].update({'Params': self.params})
         return json.dumps(self.json_dict)
 
-    def _parse_response(self, data):
+    def handle(self):
+        return self._parse_response(self.request.send(self.get_request_data(), self.content_type))
+
+    @staticmethod
+    def _parse_response(data):
         json_dict = json.loads(data, encoding='utf-8')
         is_error = json_dict['Error'] is not None
         if is_error:
@@ -143,48 +116,3 @@ class MobilyApiJsonRequestHandler(MobilyApiHttpRequestHandler):
         for key, value in json_dict['Data'].iteritems():
             response.add_data(key, value)
         return response
-
-
-class MobilyApiXmlRequestHandler(MobilyApiHttpRequestHandler):
-    def __init__(self, auth=None, request=MobilyApiRequest(api_end_point='/api/xml/')):
-        super(MobilyApiXmlRequestHandler, self).__init__(request=request)
-        self.content_type = 'xml'
-        self.params = ET.Element('MobilySMS')
-        self.add_auth(auth)
-
-    def add_auth(self, auth):
-        if isinstance(auth, MobilyAuth):
-            ET.SubElement(self.params, 'Auth', attrib={'mobile': auth.mobile_number, 'password': auth.password})
-
-    def set_api_method(self, method_name):
-        self.add_parameter('Method', method_name)
-
-    def add_parameter(self, key, value):
-        if value is not None:
-            ET.SubElement(self.params, key).text = value
-
-    def get_request_data(self):
-        return ET.tostring(self.params, 'utf-8')
-
-    def _parse_response(self, data):
-        tree = ET.fromstring(data)
-        response = MobilyApiResponse(tree.find('Status').text, tree.find('ResponseStatus').text)
-        error_element = self._element_to_dict(tree.find('Error'))
-        is_error = len(list(error_element)) > 0
-        data_or_error = 'Error' if is_error else 'Data'
-        response_data = self._element_to_dict(tree.find(data_or_error))
-        if is_error:
-            raise MobilyApiError(response_data['ErrorCode'], response_data['MessageAr'], response_data['MessageEn'])
-        for key, value in response_data.iteritems():
-            response.add_data(key, value)
-        return response
-
-    @staticmethod
-    def _element_to_dict(element):
-        data = {}
-        for child in element:
-            if len(list(child)) > 0:
-                data.update({child.tag: MobilyApiXmlRequestHandler._element_to_dict(child)})
-            else:
-                data.update({child.tag: child.text})
-        return data
